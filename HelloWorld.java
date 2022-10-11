69bf6a6537e50dbd0c82290d4af8ee8c8d3d0a7f
wulandnn <82815919+wulandnn@users.noreply.github.com>
2022-10-11 09:42:08 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     * Program pertama saya, Wulandari Nurhasanah
    */
    public static void main(String[] args){
-     //Menampilkan kata "Welcome to Java Programming" dilayar
-      System.out.println("Welcome to Java Programming");
+     //Menampilkan kata "HelloWorld" dilayar
+      System.out.println("HelloWorld");
     }
 }
