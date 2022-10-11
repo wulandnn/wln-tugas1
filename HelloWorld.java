803b86109a74383df1a491621cd7097f36b61570
wulandnn <82815919+wulandnn@users.noreply.github.com>
2022-10-11 09:39:22 +0700

    Upload Codingan HelloWorld menggunakan bahasa Java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+   /**
+    * Program pertama saya, Wulandari Nurhasanah
+   */
+   public static void main(String[] args){
+     //Menampilkan kata "Welcome to Java Programming" dilayar
+      System.out.println("Welcome to Java Programming");
+    }
+}
